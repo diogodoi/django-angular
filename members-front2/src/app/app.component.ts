c7e6f956c3454c0a1ced2a96812a19224be604e4
diogodoi <diogoogodoi@gmail.com>
2020-11-04 14:10:56 -0300

    Renomeando temporariamente a pasta

---
+++ members-front2/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'members-front';
+
+  members = [
+    {name:'Member 01 ', id: 1 , surname:'ciclano1', photo:'http://www.minhaapp.com/photo1'},
+    {name:'Member 02 ', id: 3 , surname:'ciclano2', photo:'http://www.minhaapp.com/photo2'},
+    {name:'Member 02 ', id: 3 , surname:'ciclano3', photo:'http://www.minhaapp.com/photo3'},
+  ];
+
+  constructor(private api:ApiService){
+      this.getMembers();
+  }  
+  getMembers = () =>{
+    this.api.getAllMembers().subscribe(
+      data => {
+        this.members = data
+      },
+      error => {
+        console.log("error");
+      }      
+    );
+  };
+  memberClicked = (member) => {
+    
+    this.api.getMember(member.id).subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log("error");
+      }      
+    );
+
+  };
+}
+  
